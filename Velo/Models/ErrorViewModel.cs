539a62bc950339d27381f8fcbb9067ea7e8abf4e
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-06-04 20:08:26 -0300

    Adicionados autenticacao/registro por contas de email, do google e do facebook

---
+++ Velo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Velo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
