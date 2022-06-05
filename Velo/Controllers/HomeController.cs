e78a6db8bbfb7be40894d2bb8b90cb90effe0037
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-06-05 20:09:40 -0300

    Atualizados nomes dos locais de suporte e informações das UIs

--- Velo/Controllers/HomeController.cs
+++ Velo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,8 @@ namespace Velo.Controllers
             _logger = logger;
         }
 
+
+        [Authorize]
         public IActionResult Index()
         {
             return View();
